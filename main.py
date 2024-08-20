78f22f30a6743af42b2cc1fa871b4a3df2f64bbc
Ahsen Tahir <147121714+AhsenTahir@users.noreply.github.com>
2024-08-20 23:42:57 +0500

    Add weights,Tracker class,Player assignment class

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from utils import read_video, save_video
-
+from tracker import Tracker
 
 def main():
     #reading the video
     video_frames = read_video("videos/1.mp4")
-    
+    tracker=Tracker('')
     
    #save the video
     save_video(video_frames, "output_videos/output_video3.avi")
---
+++ team_assigner/team_assigner.py
@@ -0,0 +1,5 @@
+class TeamAssigner:
+     def __init__(self):
+        self.team_colors = {}
+        self.player_team_dict = {}#player_id:team
+        pass
