11fa831e0f3541aebd2475d08c69e12e82a1a4bc
Ahsen Tahir <147121714+AhsenTahir@users.noreply.github.com>
2024-08-20 23:44:48 +0500

    Update main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tracker import Tracker
 def main():
     #reading the video
     video_frames = read_video("videos/1.mp4")
-    tracker=Tracker('')
+    tracker=Tracker('models/best_github.pt')
     
    #save the video
     save_video(video_frames, "output_videos/output_video3.avi")
