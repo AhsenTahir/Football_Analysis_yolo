dd1d222fb65a3384292519050a121c0cb3003bb7
Ahsen Tahir <147121714+AhsenTahir@users.noreply.github.com>
2024-08-21 20:20:12 +0500

    Update team assigner and main

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def main():
     video_frames = read_video("videos/1.mp4")
     tracker=Tracker('models/best_github.pt')
     
+    tracks=tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/tracks.pkl')
+
+    team_assigner=TeamAssigner()
+
+    team_assigner.assign_team_color(video_frames[0],tracks["player"][0])
+
+    for frame_num,player_track in enumerate(tracks["player"]):
+        for player_id,track in player_track.items():
+           team=team_assigner.get_player_team(video_frames[frame_num],track["bbox"],player_id)
+           track['players'][frame_num][player_id]['team']=team
+           track['players'][frame_num][player_id]['team_color']=team_assigner.team_colors[team]
    #save the video
     save_video(video_frames, "output_videos/output_video3.avi")
 
